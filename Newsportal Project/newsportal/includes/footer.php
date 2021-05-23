fca64578bb8dddd0d6679f85657efd778d4616b1
Bamma Nabin <nabinbamma99@gmail.com>
2021-05-23 13:16:27 +0545

    AMINP has been created.

---
+++ Newsportal Project/newsportal/includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Nabin Bamma 2021</p>
+      </div>
+      <!-- /.container -->
+    </footer>
